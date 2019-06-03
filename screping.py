a23d3352c25d6306046f843e9493aa39c12febcf
Rupasharma18 <43802854+Rupasharma18@users.noreply.github.com>
2019-06-03 10:35:06 +0530

    Update screping.py

--- screping.py
+++ screping.py
@@ -11,6 +11,7 @@ coursesUrl = saralUrl+ '/courses'
 coursesUrlCall= saralRequest(coursesUrl)
  
 print  "**********************************WELCOME TO SARAL**********************************"
+# all course print here
 def displayCourse():
     courseID = []
     index = 0
@@ -23,13 +24,14 @@ def displayCourse():
         index= index+1
     return courseID
 courseID = displayCourse()
-
+# choose course which you want to read.
 select_course = int(input("choose your courses no.  :- "))
 show_select_course = courseID[select_course]
 print "***************************************WELCOME TO EXERCISE*************************************"
+# in this website get  all the course's exercises
 ExerciseUrl = coursesUrl + '/' + str(show_select_course) +'/exercises'
 ExerciseUrlCall  = saralRequest(ExerciseUrl)
-
+# this funstion inside execises display. 
 childExercise = []
 slug = []
 def displayExercise():
@@ -45,13 +47,13 @@ def displayExercise():
         Ex_index = Ex_index + 1
     return slug    
 displayExercise()
-
+# choose one exercise.
 selectExercise =  int(input("choose your exercise no. :- "))
 show_select_exercise = slug[selectExercise]
 slugUrl = coursesUrl + '/' + str(show_select_course) + '/exercise/getBySlug?slug=' + show_select_exercise
 slugUrlCall = saralRequest(slugUrl)
 print (slugUrlCall['content'])
-
+# this function show the exercise's inside exercise.
 def displayChildExercise():
     child_slug = []
     i = 0
@@ -75,7 +77,7 @@ def display():
     store_content = childSlugUrlCall['content']
     print (store_content)
 display()
-
+# this function for next execise.
 def contentNext():    
     user_input = int(input("enter your num question"))
     slug_list = child_slug[user_input+-1] 
@@ -84,7 +86,7 @@ def contentNext():
     nextContent = urlCall['content']
     print (nextContent)
    
-
+# this function previous execise.
 def contentPrevious():
     userInput = int(input("enter your num question"))
     slugList = child_slug[userInput+-1] 
@@ -93,7 +95,7 @@ def contentPrevious():
     previousContent = urlCall['content']
     print (previousContent)
    
-
+# here calling the next, pervious function and exit.
 while True:
     choose= raw_input("Enter 'n' to go to next exercise or 'p' to go to previous exercise or to exit enter e key :- ")
     if choose == 'n':
